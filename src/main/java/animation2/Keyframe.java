317f3e810f457e3bec118c30a42c6162f2490559
Benjamin Schmid <bene.schmid@gmail.com>
2017-04-12 09:15:06 +0200

    Some experiments in Keyframe's main.

--- src/main/java/animation2/Keyframe.java
+++ src/main/java/animation2/Keyframe.java
@@ -1,5 +1,9 @@
 package animation2;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public class Keyframe implements Comparable<Keyframe> {
 
 	public static int UNSET = Integer.MIN_VALUE;
@@ -13,10 +17,6 @@ public class Keyframe implements Comparable<Keyframe> {
 	public float dx, dy, dz;
 	public double angleX, angleY, angleZ;
 	public int bbx, bby, bbz, bbw, bbh, bbd;
-
-	public static void main(String[] args) throws Exception {
-	}
-
 
 	public Keyframe(int frame) {
 		this.frame = frame;
@@ -48,6 +48,43 @@ public class Keyframe implements Comparable<Keyframe> {
 		this.bbw = bbw;
 		this.bbh = bbh;
 		this.bbd = bbd;
+	}
+
+	public static void main(String[] args) throws Throwable {
+		Keyframe kf = new Keyframe(2);
+		kf.renderingSettings = new RenderingSettings[] {
+				new RenderingSettings(0, 0, 0, 0, 0, 0),
+				new RenderingSettings(0, 0, 0, 0, 0, 0),
+		};
+
+		Class<?> clazz = Keyframe.class;
+		Field[] fields = clazz.getDeclaredFields();
+		for(Field f : fields) {
+			if(Modifier.isStatic(f.getModifiers())) {
+				System.out.println("...Skipping static field " + f.getName());
+			}
+			else if(Modifier.isPrivate(f.getModifiers())) {
+				System.out.println("...Skipping private field " + f.getName());
+			}
+			else if(f.getType().isArray()) {
+				System.out.println("isArray");
+				int l = Array.getLength(f.get(kf));
+				for(int i = 0; i < l; i++) {
+					Object o = Array.get(f.get(kf), i);
+					Class<?> clazz2 = o.getClass();
+					Field[] fields2 = clazz2.getDeclaredFields();
+					for(Field f2 : fields2) {
+						System.out.println(f2.getName() + ": " + f2.get(o));
+					}
+				}
+			}
+			else {
+				System.out.println(f.getName() + ": " + f.get(kf) + " (" + f.getType() + ") isInteger? " + (f.getType() == Integer.TYPE));
+			}
+		}
+		clazz.getField("dz").set(kf, 2);
+		int i = Integer.MAX_VALUE;
+		System.out.println((float)i);
 	}
 
 	public int getFrame() {
